02931198fbb32b73f53a48ae0d39b8414cea28d0
MehdiMatinfar <mehdi.matinfar1397@gmail.com>
2023-06-18 16:29:49 +0330

    three

---
+++ src/test/test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class test {
+    public static void main(String[] args) {
+        
+        System.out.println("Hi");
+
+
+
+        // this code is on test branch
+            int n = 3;
+
+        //---
+    }
+}
